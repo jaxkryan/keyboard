e1d406b9e91c58285dc5377d7fc5b46d9cccc696
hungNT <hungnthe176686@fpt.edu.vn>
2023-11-08 16:25:58 +0700

    Add UI Edit order

--- KeyboardVN/Models/Order.cs
+++ KeyboardVN/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KeyboardVN.Models
 {
@@ -12,14 +13,31 @@ namespace KeyboardVN.Models
 
         public int Id { get; set; }
         public int UserId { get; set; }
+
+        [Display(Name = "Receiver")]
         public string? Receiver { get; set; }
+        [Display(Name = "Street")]
         public string? ShipStreet { get; set; }
+
+        [Display(Name = "City")]
         public string? ShipCity { get; set; }
+
+        [Display(Name = "Province")]
         public string? ShipProvince { get; set; }
+
+        [Display(Name = "Country")]
         public string? ShipCountry { get; set; }
+
+        [Display(Name = "Email Address")]
         public string? ShipEmail { get; set; }
+
+        [Display(Name = "Phone")]
         public string? ShipPhone { get; set; }
+
+        [Display(Name = "Status")]
         public string Status { get; set; } = null!;
+
+        [Display(Name = "Created Time")]
         public DateTime? CreatedTime { get; set; }
 
         public virtual User? User { get; set; } = null!;
