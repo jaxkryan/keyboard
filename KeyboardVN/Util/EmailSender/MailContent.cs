33fedd31084c6f87f6815c3136e30126cf70ff5b
hungNT <hungnthe176686@fpt.edu.vn>
2023-11-11 08:41:47 +0700

    Fix feat send image in email

--- KeyboardVN/Util/EmailSender/MailContent.cs
+++ KeyboardVN/Util/EmailSender/MailContent.cs
@@ -5,6 +5,7 @@
         public string To { get; set; }              // Địa chỉ gửi đến
         public string Subject { get; set; }         // Chủ đề (tiêu đề email)
         public string Body { get; set; }            // Nội dung (hỗ trợ HTML) của email
+        public string ImagePath { get; set; }
 
     }
 }
